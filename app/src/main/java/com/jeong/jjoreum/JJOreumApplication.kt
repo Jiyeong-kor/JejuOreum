c8f66cdf8cc4c9916ea323a01354184e5d9fb0c5
Jiyeong <jeongjiyeong486@gmail.com>
2025-09-01 20:03:47 +0900

    feat(app): Android 16 Adaptive UI 가이드에 맞춰 회전 지원

--- app/src/main/java/com/jeong/jjoreum/JJOreumApplication.kt
+++ app/src/main/java/com/jeong/jjoreum/JJOreumApplication.kt
@@ -1,11 +1,7 @@
 package com.jeong.jjoreum
 
-import android.annotation.SuppressLint
-import android.app.Activity
 import android.app.Application
 import android.content.Context
-import android.content.pm.ActivityInfo
-import android.os.Bundle
 import coil3.ImageLoader
 import coil3.SingletonImageLoader
 import dagger.hilt.EntryPoint
@@ -32,30 +28,10 @@ class JJOreumApplication : Application(), SingletonImageLoader.Factory {
 
     companion object {
         private lateinit var jjOreumApplication: JJOreumApplication
-        fun getInstance(): JJOreumApplication = jjOreumApplication
     }
 
     override fun onCreate() {
         super.onCreate()
         jjOreumApplication = this
-
-        // 화면을 세로 모드로 고정
-        settingScreenPortrait()
-    }
-
-    private fun settingScreenPortrait() {
-        registerActivityLifecycleCallbacks(object : ActivityLifecycleCallbacks {
-            @SuppressLint("SourceLockedOrientationActivity")
-            override fun onActivityCreated(activity: Activity, p1: Bundle?) {
-                activity.requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
-            }
-
-            override fun onActivityStarted(p0: Activity) {}
-            override fun onActivityResumed(p0: Activity) {}
-            override fun onActivityPaused(p0: Activity) {}
-            override fun onActivityStopped(p0: Activity) {}
-            override fun onActivitySaveInstanceState(p0: Activity, p1: Bundle) {}
-            override fun onActivityDestroyed(p0: Activity) {}
-        })
     }
 }
