1a48af05fa4796bcd8cdf03e6ca4b18e1a53d112
Jiyeong <jeongjiyeong486@gmail.com>
2025-09-11 04:01:48 +0900

    refactor(core-navigation): core 패키지명으로 변경

--- core/navigation/build.gradle.kts
+++ core/navigation/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 android {
-    namespace = "com.jeong.navigation"
+    namespace = "com.jeong.core.navigation"
     compileSdk = 36
 
     defaultConfig {
--- core/navigation/src/main/java/com/jeong/navigation/NavigationPlaceholder.kt
+++
@@ -1,4 +0,0 @@
-package com.jeong.navigation
-
-@Suppress("unused")
-object NavigationPlaceholder
